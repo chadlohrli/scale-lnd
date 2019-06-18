dfd0effff625fd3bf9869b860868b12972da88e3
chadlohrli <chad.david.lohrli@gmail.com>
2019-06-17 19:22:05 -0700

    remove unnecessary files

--- genseed.py
+++
@@ -1,7 +0,0 @@
-import base64, codecs, json, requests
-import os
-url = 'https://localhost:8001/v1/genseed'
-cert_path = os.path.expanduser('~/.lnd/tls.cert')
-r = requests.get(url,verify=cert_path)
-print(r.json()['cipher_seed_mnemonic'])
-
