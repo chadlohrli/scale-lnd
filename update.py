fb03365c077d5fa84e4995370fab9f374a6099ab
cfxdev <ec2-user@ip-10-0-0-211.ec2.internal>
2019-06-21 23:48:13 +0000

    add github webhook server

---
+++ update.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+import os
+import requests
+import string
+import random
+import base64, codecs, json
+import time
+
+
+app = Flask(__name__)
+
+@app.route('/update', methods=['GET', 'POST'])
+def update():
+	
+	os.system('cd ../ && rm lndserver.py')
+	os.system('cp lndserver.py ../')
+	os.system('sudo systemctl restart lndserver')
+	
+	return "ok"
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=5001)
